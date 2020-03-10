b1d1b6f3347f0ee13e16d9532682c3e5f5a54347
Oliver Burt <oliverburt@Emanuels-iMac-2.local>
2020-03-10 10:23:55 +0000

    persist logged in status

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import Products from './components/Products';
-import ProductAdmin from './components/ProductAdmin';
-import LogIn from './components/auth/LogIn';
-import Register from './components/auth/Register';
-import ForgotPassword from './components/auth/ForgotPassword';
-import ForgotPasswordVerification from './components/auth/ForgotPasswordVerification';
-import ChangePassword from './components/auth/ChangePassword';
-import ChangePasswordConfirm from './components/auth/ChangePasswordConfirm';
-import Welcome from './components/auth/Welcome';
-import Footer from './components/Footer';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Products from "./components/Products";
+import ProductAdmin from "./components/ProductAdmin";
+import LogIn from "./components/auth/LogIn";
+import Register from "./components/auth/Register";
+import ForgotPassword from "./components/auth/ForgotPassword";
+import ForgotPasswordVerification from "./components/auth/ForgotPasswordVerification";
+import ChangePassword from "./components/auth/ChangePassword";
+import ChangePasswordConfirm from "./components/auth/ChangePasswordConfirm";
+import Welcome from "./components/auth/Welcome";
+import Footer from "./components/Footer";
+import { Auth } from "aws-amplify";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
 library.add(faEdit);
 
 class App extends Component {
@@ -22,6 +23,19 @@ class App extends Component {
     isAuthenticated: false,
     user: null
   };
+
+  async componentDidMount() {
+    try {
+      const session = await Auth.currentSession();
+      console.log(session);
+      this.setAuthStatus(true);
+      const user = await Auth.currentAuthenticatedUser();
+      console.log(user);
+      this.setUser(user);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   setAuthStatus = isAuthenticated => this.setState({ isAuthenticated });
 
@@ -40,24 +54,60 @@ class App extends Component {
           <div>
             <Navbar auth={authProps} />
             <Switch>
-              <Route exact path="/" render={props => <Home {...props} auth={authProps} />} />
-              <Route exact path="/products" render={props => <Products {...props} auth={authProps} />} />
-              <Route exact path="/admin" render={props => <ProductAdmin {...props} auth={authProps} />} />
-              <Route exact path="/login" render={props => <LogIn {...props} auth={authProps} />} />
-              <Route exact path="/register" render={props => <Register {...props} auth={authProps} />} />
-              <Route exact path="/forgotpassword" render={props => <ForgotPassword {...props} auth={authProps} />} />
+              <Route
+                exact
+                path="/"
+                render={props => <Home {...props} auth={authProps} />}
+              />
+              <Route
+                exact
+                path="/products"
+                render={props => <Products {...props} auth={authProps} />}
+              />
+              <Route
+                exact
+                path="/admin"
+                render={props => <ProductAdmin {...props} auth={authProps} />}
+              />
+              <Route
+                exact
+                path="/login"
+                render={props => <LogIn {...props} auth={authProps} />}
+              />
+              <Route
+                exact
+                path="/register"
+                render={props => <Register {...props} auth={authProps} />}
+              />
+              <Route
+                exact
+                path="/forgotpassword"
+                render={props => <ForgotPassword {...props} auth={authProps} />}
+              />
               <Route
                 exact
                 path="/forgotpasswordverification"
-                render={props => <ForgotPasswordVerification {...props} auth={authProps} />}
+                render={props => (
+                  <ForgotPasswordVerification {...props} auth={authProps} />
+                )}
               />
-              <Route exact path="/changepassword" render={props => <ChangePassword {...props} auth={authProps} />} />
+              <Route
+                exact
+                path="/changepassword"
+                render={props => <ChangePassword {...props} auth={authProps} />}
+              />
               <Route
                 exact
                 path="/changepasswordconfirm"
-                render={props => <ChangePasswordConfirm {...props} auth={authProps} />}
+                render={props => (
+                  <ChangePasswordConfirm {...props} auth={authProps} />
+                )}
               />
-              <Route exact path="/welcome" render={props => <Welcome {...props} auth={authProps} />} />
+              <Route
+                exact
+                path="/welcome"
+                render={props => <Welcome {...props} auth={authProps} />}
+              />
             </Switch>
             <Footer />
           </div>
